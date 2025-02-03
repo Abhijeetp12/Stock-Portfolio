66aaa93eb3e9d5c2333335b114cc851d6d03b3e6
Abhijeetp12 <130688454+Abhijeetp12@users.noreply.github.com>
2025-02-03 23:29:18 +0530

    Create PortfolioSchema.js

---
+++ Backend/PortfolioSchema.js
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    google_ID VARCHAR(255) 
+);
+CREATE TABLE user_stocks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    stock_name VARCHAR(255) NOT NULL,
+    ticker VARCHAR(20) ,
+    quantity INT DEFAULT 1, 
+    price DOUBLE ,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
